97f01ecc8e68b3de25290048309849f1e6bc1c8a
MaksimK30 <makskuzmih@mail.ru>
2023-09-22 14:08:16 +0300

    Внесены изменения в функцию At, Настроена функция PushBack, исправлены прочие баги

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include "vector.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Vector<int> v;
+    cout << ((v.At(-4) != nullptr) ? v.At(-4) : 0 ) << " " << ((v.At(0) != nullptr) ? *v.At(0) : 0 ) << " " << ((v.At(2) != nullptr) ? *v.At(2) : 0 ) << endl;
+    cout << v.Capacity() << endl;
+    v.PushBack(4);
+    v.PushBack(5);
+    v.PushBack(6);
+    v.PushBack(7);
+    v.PushBack(8);
+    v.PushBack(9);
+    v.PushBack(10);
+    v.PushBack(11);
+    v.PushBack(12);
+    v.PushBack(13);
+    v.PushBack(14);
+    v.PushBack(15);
+    cout << ((v.At(-4) != nullptr) ? v.At(-4) : 0 ) << " " << ((v.At(0) != nullptr) ? *v.At(0) : 0 ) << " " << ((v.At(2) != nullptr) ? *v.At(2) : 0 ) << endl;
+    cout << v.Capacity() << endl;
     return 0;
 }
--- vector.h
+++ vector.h
@@ -5,28 +5,108 @@ template <class T>
 class Vector
 {
 public:
-    Vector();
-    void Assign(Vector<T> const & newVector);
-    T& At(int const position);
-    T& Back();
-    int Capacity();
-    void Clear();
-    bool Empty();
-    T& Front();
-    void Insert(int const position, T const value);
-    void Insert(int const position, T const * values[]);
-    void Erase(int const position);
-    void Erase(int const start, int const end);
-    T PopBack();
-    void PushBack(T const & value);
-    void PushFront(T const & value);
-    int Size();
+    Vector(){
+        size = 0;
+        capacity = 10;
+        internalArr = new T[capacity]{};
+    }
+
+    //Удаляет вектор и переносит его элементы в новый вектор
+    void Assign(Vector<T> const & newVector){
+
+    }
+
+    //Возвращает указатель на элемент в выбранной позиции
+    T* At(int const position){
+        if(position < 0 || position > size - 1){
+            return nullptr;
+        }
+
+        return &internalArr[position];
+    }
+
+    //Возвращает ссылку на последний элемент
+    T& Back(){
+
+    }
+
+    //Возвращает количество элементов, которое может быть добавлено//без повторного выделения памяти
+
+    int Capacity(){
+        return capacity;
+    }
+
+    //Очищает вектор
+    void Clear(){
+        size = 0;
+        capacity = 10;
+        internalArr = new T[capacity]{};
+    }
+
+    //Проверяет пуст ли вектор
+    bool Empty(){
+        return size == 0 ? true : false;
+    }
+
+    //Возвращает первый элемент вектора
+    T& Front(){
+
+    }
+
+    //Вставляет элемент в указанную позицию
+    void Insert(int const position, T const value){
+
+    }
+
+    //Вставляет элементы, начиная с указанной позиции
+    void Insert(int const position, T const * values[]){
+
+    }
+
+    //Удаляет элемент в указанной позиции
+    void Erase(int const position){
+
+    }
+
+    //Удаляет элементы, начиная с указанной позиции
+    void Erase(int const start, int const end){
+
+    }
+
+    //Удаляет элементы в указанном диапазоне(включительно)
+    T PopBack(){
+
+    }
+
+    //Вставляет элемент в конец
+    void PushBack(T const value){
+        CheckCapacity(size + 1);
+        internalArr[size == 0 ? 0 : size + 1] = value;
+        size++;
+    }
+
+    //Вставляет элемент в начало
+    void PushFront(T const value){
+
+    }
+
+    //Возвращает количество элементов в векторе
+    int Size(){
+        return size;
+    }
 
 
 private:
     int capacity;
     int size;
-    T internalArr[];
+    T* internalArr;
+
+    //Расширяет вектор при необходимости
+    void CheckCapacity(int newSize){
+        if(capacity < newSize){
+            capacity = static_cast<int>(capacity * 1.5);
+        }
+    }
 };
 
 #endif // VECTOR_H
