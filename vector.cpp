7921b57dee089e88af9975caa320ff890ac94d93
MaksimK30 <makskuzmih@mail.ru>
2023-09-22 10:36:45 +0300

    Подготовлен заголовочный файл и файл исходного кода

---
+++ vector.cpp
@@ -0,0 +1,99 @@
+#include "vector.h"
+
+template <class T>
+Vector<T>::Vector()
+{
+
+}
+
+//Удаляет вектор и переносит его элементы в новый вектор
+template <class T>
+void Vector<T>::Assign(Vector<T> const & newVector){
+
+}
+
+//Возвращает ссылку на элемент в выбранной позиции
+template <class T>
+T& Vector<T>::At(int const position){
+
+}
+
+//Возвращает ссылку на последний элемент
+template <class T>
+T& Vector<T>::Back(){
+
+}
+
+//Возвращает количество элементов, которое может быть добавлено
+//без повторного выделения памяти
+template <class T>
+int Vector<T>::Capacity(){
+
+}
+
+//Очищает вектор
+template <class T>
+void Vector<T>::Clear(){
+
+}
+
+//Проверяет пуст ли вектор
+template <class T>
+bool Vector<T>::Empty(){
+
+}
+
+//Возвращает первый элемент вектора
+template <class T>
+T& Front(){
+
+}
+
+//Вставляет элемент в указанную позицию
+template <class T>
+void Vector<T>::Insert(int const position, T const value){
+
+}
+
+//Вставляет элементы, начиная с указанной позиции
+template <class T>
+void Vector<T>::Insert(int const position, T const * values[]){
+
+}
+
+//Удаляет элемент в указанной позиции
+template <class T>
+void Vector<T>::Erase(int const position){
+
+}
+
+//Удаляет элементы, начиная с указанной позиции
+template <class T>
+void Vector<T>::Erase(int const start, int const end){
+
+}
+
+//Удаляет элементы в указанном диапазоне(включительно)
+template <class T>
+T Vector<T>::PopBack(){
+
+}
+
+//Вставляет элемент в конец
+template <class T>
+void Vector<T>::PushBack(T const & value){
+
+}
+
+//Вставляет элемент в начало
+template <class T>
+void Vector<T>::PushFront(T const & value){
+
+}
+
+//Возвращает количество элементов в векторе
+template <class T>
+int Vector<T>::Size(){
+
+}
+
---
+++ vector.h
@@ -0,0 +1,32 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+template <class T>
+class Vector
+{
+public:
+    Vector();
+    void Assign(Vector<T> const & newVector);
+    T& At(int const position);
+    T& Back();
+    int Capacity();
+    void Clear();
+    bool Empty();
+    T& Front();
+    void Insert(int const position, T const value);
+    void Insert(int const position, T const * values[]);
+    void Erase(int const position);
+    void Erase(int const start, int const end);
+    T PopBack();
+    void PushBack(T const & value);
+    void PushFront(T const & value);
+    int Size();
+
+
+private:
+    int capacity;
+    int size;
+    T InternalArr[];
+};
+
+#endif // VECTOR_H
